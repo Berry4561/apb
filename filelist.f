cc110a3a85b7d28cf53c5181f4709833c616e164
Berry4561 <38258401+Berry4561@users.noreply.github.com>
2024-10-09 11:11:41 +0530

    Add files via upload
    filelist for vcs compilation

---
+++ filelist.f
@@ -0,0 +1,10 @@
++incdir+/home/ip/shared/external/uvm/1.2/77379836/snps/2023.03-SP2-5/src
++incdir+design
++incdir+verif
++incdir+verif/tb_top
+
+
+# List your source files below
+/home/ip/shared/external/uvm/1.2/77379836/snps/2023.03-SP2-5/src/uvm_pkg.sv
+design/dut_includes.sv
+verif/tb_includes.sv
