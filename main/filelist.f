106175112310c6a12401406d6d822568f9f567c8
Aditya Prusty <aprusty@pdx-xterm-153.nvidia.com>
2024-10-17 02:44:11 -0700

    Change in heirarchy

--- main/filelist.f
+++ main/filelist.f
@@ -6,5 +6,5 @@
 
 # List your source files below
 /home/ip/shared/external/uvm/1.2/77379836/snps/2023.03-SP2-5/src/uvm_pkg.sv
-design/dut_includes.sv
-verif/tb_includes.sv
+./design/dut_includes.sv
+./verif/tb_includes.sv
